7be2c9151fe41a88338b10206b73d9bead02d225
ZeroMortalZen <79844633+ZeroMortalZen@users.noreply.github.com>
2021-11-03 13:11:42 +0000

    Added files

---
+++ app.js
@@ -0,0 +1,76 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var validator = require('validator');
+
+
+var indexRouter = require('./routes/index');
+// import
+var indexRouter2 = require('./routes/index2');
+
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// use the route
+app.use('/index2', indexRouter2);
+
+app.use('/users', usersRouter);
+
+
+app.get('/login',function(req,res){
+
+  console.log("hello");
+  res.send("all ok")
+
+});
+
+app.post('/login',function(req,res){
+	
+	var user_name=req.body.username;
+	
+	console.log("User name = "+user_name);
+	
+	
+	console.log(validator.isEmpty(user_name));
+	
+	
+	res.send("all ok " + user_name);
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
